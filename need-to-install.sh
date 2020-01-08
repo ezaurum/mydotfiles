157abcce3d369db0395f10030d89904459d508ab
조석규 <ezaurum@gmail.com>
2020-01-08 22:31:41 +0900

    Add path

--- need-to-install.sh
+++ need-to-install.sh
@@ -1,2 +1,7 @@
-yay -S gitflow-avh autoenv-git
+#! /bin/zsh
+
+curl -O https://bootstrap.pypa.io/get-pip.py &&
+python get-pip.py --user &&
+rm get-pip.py &&
+yay -S gitflow-avh autoenv-git &&
 curl https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf -o ~/.tmux.conf
