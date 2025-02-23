8bbe6c7af52175458a57662a70c80836d6a0be5c
조석규 <ezaurum@gmail.com>
2025-02-24 01:54:49 +0900

    feat: Add plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
 require("config.lazy")
 require("keymaps")
+require("nvim-tree").setup()
 vim.g.mapleader = " "
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- .config/nvim/lua/plugins/edit.lua
+++ .config/nvim/lua/plugins/edit.lua
@@ -1,6 +1,7 @@
 return {
 	  -- 기본 편집 기능
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-tree/nvim-tree.lua" },
   { "neovim/nvim-lspconfig" },
   { "williamboman/mason.nvim", config = true },
   { "hrsh7th/nvim-cmp" },
